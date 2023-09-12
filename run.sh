57abd76397c8aef19a5e594ec02e42a104cab886
Marie <marie@kaifa.ch>
2023-09-13 00:50:34 +0200

    Add check

--- run.sh
+++ run.sh
@@ -4,5 +4,28 @@ for i in {0..9}; do
     test -S $XDG_RUNTIME_DIR/discord-ipc-$i || ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
 done
 
+declare -a EXTRA_FLAGS=(--no-sandbox)
+
+# Display Socket
+if [[ $XDG_SESSION_TYPE == "wayland" ]]
+then
+    WAYLAND_SOCKET=${WAYLAND_DISPLAY:-"wayland-0"}
+
+    if [[ -e "$XDG_RUNTIME_DIR/${WAYLAND_SOCKET}" ]]
+    then
+        echo "Wayland socket is available, running natively on Wayland."
+        echo "To disable, remove the --socket=wayland permission."
+        EXTRA_FLAGS+=(--ozone-platform=wayland)
+    else
+        echo "Wayland socket not available, running through Xwayland."
+    fi
+    if [[ -c /dev/nvidia0 ]]
+    then
+        echo "Using NVIDIA on Wayland, applying workaround"
+        EXTRA_FLAGS+=(--disable-gpu-sandbox)
+    fi
+
+fi
+
 cd /app/lib/tidal-hifi
-exec ./tidal-hifi --no-sandbox "$@"
+exec ./tidal-hifi "${FLAGS[@]}" "$@"
